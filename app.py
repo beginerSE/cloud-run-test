9b5b503039ddc62ad1dc225d71f0ae01f18a5d87
ミナピ <42573936+beginerSE@users.noreply.github.com>
2025-11-12 02:55:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello Cloud Run!"
+    return "OK"
 
 if __name__ == "__main__":
+    # ローカル検証用。Cloud Run では gunicorn が使われる
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
