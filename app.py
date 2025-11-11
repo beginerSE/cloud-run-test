aef1ad17e29dc08d104e5537ff3aa3a04c13307f
ミナピ <42573936+beginerSE@users.noreply.github.com>
2025-11-12 01:45:59 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return "Hello Cloud Run!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
